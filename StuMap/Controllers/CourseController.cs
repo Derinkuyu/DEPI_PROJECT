72606c71d6f1ebd40ed812728a67725fbc45485f
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2026-05-11 20:18:33 +0300

    finishing All courses design for contributer

---
+++ StuMap/Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StuMap.Managers;
+
+namespace StuMap.Controllers
+{
+    public class CourseController : Controller
+    {
+        ICourseManager courseRepo;
+        public CourseController(ICourseManager courseRepo)
+        {
+            this.courseRepo = courseRepo;
+        }
+        public IActionResult Index()
+        {
+            return View(courseRepo.GetAll());
+        }
+
+    }
+}
