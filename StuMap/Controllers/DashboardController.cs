1c337a378083c04f4684d42f102845f7bd383a8a
mosaed <you@example.com>
2026-05-17 05:55:46 +0300

    DashBoard

---
+++ StuMap/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StuMap.Managers;
+
+namespace StuMap.Controllers
+{
+    public class DashboardController : Controller
+    { 
+        //Course
+        ICourseManager courseManager;
+        IRoadmapManager roadmapManager;
+
+      
+
+        public DashboardController(ICourseManager courseManager, IRoadmapManager roadmapManager)
+        {
+            this.courseManager = courseManager;
+            this.roadmapManager = roadmapManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Course = courseManager.GetAll();
+            ViewBag.RoadMaps= roadmapManager.GetAll();
+            return View();
+        }
+    }
+}
